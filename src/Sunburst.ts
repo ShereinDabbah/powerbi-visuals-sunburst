8514fd6eafff0d84494b2daab1cd0ae131848499
Nikita Grachev <v-grniki@microsoft.com>
2021-03-09 18:40:13 +0700

    Fix initial color (#49)
    * Fix inital color
    
    * Increased version
    
    * fixed empty lines

--- src/Sunburst.ts
+++ src/Sunburst.ts
@@ -555,6 +555,15 @@ export class Sunburst implements IVisual {
 
         data.total += newDataPointNode.value;
         newDataPointNode.children = [];
+      
+        
+        if (level === 1 && originParentNode.children.length > 0) {
+            const initialColor: string = this.colorPalette.getColor(name).value;
+            for (const child of originParentNode.children) {
+                const childName: string = child.value != null ? `${child.value}` : "";
+                const initialColor: string = this.colorPalette.getColor(childName).value;
+            }
+        }
 
         if (name && level === 2 && !originParentNode.objects) {
             const initialColor: string = this.colorPalette.getColor(name).value;
@@ -572,7 +581,7 @@ export class Sunburst implements IVisual {
         } else {
             newDataPointNode.color = parentColor;
         }
-
+ 
         if (originParentNode.children && originParentNode.children.length > 0) {
             for (const child of originParentNode.children) {
                 const nodeColor: string = this.getColor(
